6cf8a7658716dc8f4e5345a179ff47dd205eec53
Hansaja Sandeepa <hansajas@Direct.local>
2025-01-14 15:05:21 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,47 @@
+import { NgClass, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+export interface TodoItem {
+  id:number;
+  task:string;
+  completed:boolean;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,NgFor,NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todoapp';
+  todolist:TodoItem [] = [];
+  newTask:string = '';
+
+  addTask():void {
+    if(this.newTask.trim() !== ''){
+
+      const newTodoItem:TodoItem = {
+        id:Date.now(),
+        task:this.newTask,
+        completed:false
+      }
+
+      //console.log(this.todolist)
+
+      this.todolist.push(newTodoItem)
+      this.newTask = ''
+    }
+  }
+
+  toggleComplted(index:number):void {
+    this.todolist[index].completed =!this.todolist[index].completed
+    console.log(this.todolist);
+  }
+
+  deleteTask(id:number):void {
+    this.todolist = this.todolist.filter(item => item.id != id)
+    console.log(this.todolist)
+  }
 }
